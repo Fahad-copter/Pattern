de86b1816b68680cbc65e811827a6bc7495757f4
Fahad-copter <70465455+Fahad-copter@users.noreply.github.com>
2021-06-15 18:30:29 +0530

    pattern0.cpp
    desc.

---
+++ pattern0.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+/*  INPUT IS 5: */
+/*  DESIRED OUTPUT  */
+/* 
+
+
+*****
+****
+***   
+**       
+*      
+
+
+*/
+int main()
+{
+    int n;
+    cin>>n;
+    int i,j;
+    for(i=n;i>0;i--)
+    {
+        for(j=i;j>0;j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
